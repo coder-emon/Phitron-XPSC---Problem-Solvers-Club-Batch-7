d3273d8e104befb287631219108e75132de0ba53
Emon Howlader <developeremon.me@gmail.com>
2025-11-13 01:43:33 +0600

    Number of Smaller Problem

---
+++ Week 04 (Two Pointers, Prefix Sum, Difference Array & Fixed Size Sliding Window)/B_Number_of_Smaller.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl '\n'
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,m;
+    cin >> n >> m;
+    vector<int> a(n) ,b(m);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    for(int i = 0; i < m; i++){
+        cin >> b[i];
+    }
+
+    int l = 0, r = 0;
+    int cnt = 0;
+    vector<int> ans;
+    while(1){
+        if(l < n && a[l] < b[r]){
+            cnt++;
+            l++;
+        }else{
+            ans.push_back(cnt);
+            r++;
+        }
+        if(r == m) break;
+    }
+
+    for(int i = 0; i < ans.size(); i++){
+        cout << ans[i] << " ";
+    }
+
+    
+    return 0;
+}
